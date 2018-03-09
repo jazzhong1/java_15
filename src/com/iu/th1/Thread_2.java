3a0361aff6ea908e8994fdd68f2cd9a76c79db8f
jazzhong1 <jazzhong1@naver.com>
2018-03-09 13:51:26 +0900

    ver1.1

--- src/com/iu/th1/Thread_2.java
+++ src/com/iu/th1/Thread_2.java
@@ -6,19 +6,18 @@ public class Thread_2 extends Thread {
 	public void run() {
 		this.s2();
 	}
-	
-	
-	public void s2(){
-		for (int i = 'A'; i < 'j'; i++){
-			
+
+	public void s2() {
+		for (int i = 'A'; i < 'j'; i++) {
+
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO 자동 생성된 catch 블록
 				e.printStackTrace();
 			}
-			
-			System.out.println((char)i);
+
+			System.out.println((char) i);
 		}
 	}
 }
